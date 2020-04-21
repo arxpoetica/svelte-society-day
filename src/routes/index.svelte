840c7a1b3ffae3f867779c3e26008866781a1224
Robert Hall <langbert@gmail.com>
2020-04-20 20:59:44 -0600

    Some sample chapters (temp imagery)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,73 +1,53 @@
-<svelte:window bind:scrollY={scroll_y} bind:innerHeight={$view_height}/>
+<svelte:window bind:scrollY bind:innerHeight/>
 <svelte:head><title>Svelte Society Day - Home</title></svelte:head>
 
 <Panel/>
 
 <div class="chapters">
+
 	<Chapter1/>
+	<Chapter1/>
+	<Chapter1/>
+	<Chapter1/>
+	<Chapter1/>
+	<Chapter1/>
+	<Chapter1/>
+	<Chapter1/>
+	<Chapter1/>
+	<!-- <Chapter2/> -->
+	<!-- <Chapter3/> -->
 
-	<IntersectionObserver let:elem let:intersecting>
-		<div class="chapter chapter-2">
-			<h2>Content</h2>
-		</div>
-	</IntersectionObserver>
-
-	<IntersectionObserver let:elem let:intersecting>
-		<div class="chapter chapter-3">
-			<h2>Content</h2>
-		</div>
-	</IntersectionObserver>
-
-	<IntersectionObserver let:elem let:intersecting>
-		<div class="chapter chapter-4">
-			<h2>Content</h2>
-		</div>
-	</IntersectionObserver>
-
-	<IntersectionObserver let:elem let:intersecting>
-		<div class="chapter chapter-5">
-			<h2>Content</h2>
-		</div>
-	</IntersectionObserver>
-
-	<IntersectionObserver let:elem let:intersecting>
-		<div class="chapter chapter-6">
-			<h2>Content</h2>
-		</div>
-	</IntersectionObserver>
-
-	<IntersectionObserver let:elem let:intersecting>
-		<div class="chapter chapter-7">
-			<h2>Content</h2>
-		</div>
-	</IntersectionObserver>
-
-	<IntersectionObserver let:elem let:intersecting>
-		<div class="chapter chapter-8">
-			<h2>Content</h2>
-		</div>
-	</IntersectionObserver>
 </div>
 
 <script>
 	import { story_scroll, view_height } from '@society/js/stores.js'
 
 	import Panel from '@society/svelte/layout/Panel.svelte'
-	import IntersectionObserver from '@society/svelte/IntersectionObserver.svelte'
-	import Chapter1 from '@society/svelte/Chapter1.svelte'
 
-	let scroll_y = 0
-	$: story_scroll.set(scroll_y)
+	import IntersectionObserver from '@society/svelte/IntersectionObserver.svelte'
+
+	import Chapter1 from '@society/svelte/Chapter1.svelte'
+	// import Chapter2 from '@society/svelte/Chapter2.svelte'
+	// import Chapter3 from '@society/svelte/Chapter3.svelte'
+
+	let scrollY = 0
+	$: story_scroll.set(scrollY)
+
+	let innerHeight = 0
+	$: view_height.set(innerHeight - 100)
 </script>
 
 <style type="text/scss">
 	.chapters {
-		padding-top: 100rem;
+		padding-bottom: 100rem;
 		width: 100%;
 	}
-	.chapter {
-		width: 100%;
-		padding: 50rem;
-		color: white;
+	:global {
+		.chapter {
+			position: relative;
+			overflow: hidden;
+			display: flex;
+			width: 100%;
+		}
 	}
 </style>
